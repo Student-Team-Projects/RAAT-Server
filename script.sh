cc46c7795b73f258498b988f4c9d450cec83da0c
Justyna <jusjanna7@gmail.com>
2023-01-21 22:42:19 +0100

    Initial commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if (($# != 3))
+then
+	echo "Usage: raat-server [vnc password] [rfb port] [geometry]"
+	exit 1
+fi
+
+display=$(($2-5900))
+echo $1 | vncpasswd -f > ~/.vnc/$2.passwd
+Xvnc -geometry $3 -rfbauth ~/.vnc/$2.passwd :$display &
+DISPLAY=:$display cinnamon-session &
+
