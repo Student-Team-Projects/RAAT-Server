6bebed525fb8b649628ce2f3439773ac0bc7193e
Justyna Jaworska <jusjanna7@gmail.com>
2023-02-14 20:22:17 +0100

    The .vnc directory might not exist...

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ then
 fi
 
 display=$(($2-5900))
+mkdir -p ~/.vnc && touch ~/.vnc/$2.passwd
 echo $1 | vncpasswd -f > ~/.vnc/$2.passwd
 Xvnc -geometry $3 -rfbauth ~/.vnc/$2.passwd :$display &
 DISPLAY=:$display cinnamon-session &
