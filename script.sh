eadc5bde17ffb5a435ad74ad18b21c270086124f
Rafał Kajca <92093047+rafak1@users.noreply.github.com>
2024-01-24 18:04:31 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,15 @@
 
 if (($# != 3))
 then
-	echo "Usage: raat-server [vnc password] [rfb port] [geometry]"
-	exit 1
+    echo "Usage: raat-server [vnc password] [rfb port] [geometry]"
+    exit 1
 fi
 
 display=$(($2-5900))
+
 mkdir -p ~/.vnc && touch ~/.vnc/$2.passwd
 echo $1 | vncpasswd -f > ~/.vnc/$2.passwd
-Xvnc -geometry $3 -rfbauth ~/.vnc/$2.passwd :$display &
-DISPLAY=:$display cinnamon-session &
+setsid Xvnc -AlwaysShared -geometry $3 -rfbauth ~/.vnc/$2.passwd :$display &
+DISPLAY=:$display setsid startlxde &
+vncviewer -passwd ~/.vnc/$2.passwd  0.0.0.0:$2
 
